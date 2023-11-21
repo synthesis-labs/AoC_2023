a81974457ec1183f4feeb4a8b3d276d100ad1cde
Bhavesh Sooka <bsooka@gmail.com>
2023-11-21 18:02:08 +0200

    Something Generic

--- bhavesh/rust/src/days/day02.rs
+++ bhavesh/rust/src/days/day02.rs
@@ -1,21 +1,30 @@
 use crate::{models::aoc_answer::AocAnswer, utils::get_question_data::get_question_data};
 
-pub async fn solve() -> AocAnswer {
+pub async fn solve() -> AocAnswer<Vec<i32>> {
     let response = get_question_data(2022, 2)
         .await
         .expect("Could not get Day 2 data");
 
-    println!("{:?}", response);
-
-    let result = AocAnswer {
+    let result: AocAnswer<Vec<i32>> = AocAnswer {
         day: 2,
-        part1: part1().to_string(),
-        part2: response.len().to_string(),
+        part1: part1(),
+        part2: part2(response.len() as i32),
     };
 
     result
 }
 
-fn part1() -> i32 {
-    0
+fn part1() -> Vec<i32> {
+    let a: i32 = 77;
+    let mut b: Vec<i32> = Vec::new();
+    b.insert(0, a);
+    b.insert(0, a);
+    b
+}
+
+
+fn part2(param: i32) -> Vec<i32> {
+    let mut b: Vec<i32> = Vec::new();
+    b.insert(0, param);
+    b
 }
--- bhavesh/rust/src/models/aoc_answer.rs
+++ bhavesh/rust/src/models/aoc_answer.rs
@@ -1,16 +1,36 @@
 use std::fmt;
 
-pub struct AocAnswer {
+pub struct AocAnswer<T> {
     pub(crate) day: i32,
-    pub(crate) part1: String,
-    pub(crate) part2: String,
+    pub(crate) part1: T,
+    pub(crate) part2: T,
 }
 
-impl fmt::Display for AocAnswer {
+impl fmt::Display for AocAnswer<String> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "Day: {}", self.day)?;
-        writeln!(f, "Part 1: {}", self.part1)?;
-        writeln!(f, "Part 2: {}", self.part2)?;
+        writeln!(f, "Part 1: {}", &self.part1)?;
+        writeln!(f, "Part 2: {}", &self.part2)?;
+        Ok(())
+    }
+}
+
+impl fmt::Display for AocAnswer<Vec<i32>> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Day: {}", self.day)?;
+
+        let _ = write!(f, "Part 1: ");
+        for x in &self.part1 {
+            print!("{}, ", x);
+        }
+        let _ = writeln!(f, "");
+        
+        let _ = write!(f, "Part 2: ");
+        for x in &self.part2 {
+            print!("{}, ", x);
+        }
+        let _ = writeln!(f, "");
+
         Ok(())
     }
 }
