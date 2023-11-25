3bc3d033e9174b0263bfefff6d2b8cc88a046841
Tom Wells <drshade@gmail.com>
2023-11-25 23:36:53 +0100

    moved to submodule approach

--- tom/app/Day14.hs
+++
@@ -1,8 +0,0 @@
-module Day14 where
-
-import           AoC
-
-solve :: IO (Solution Int)
-solve = do
-  putStrLn "Workings of day 14 problems go here... 🚀"
-  pure $ NoSolution 2023 14
--- tom/app/Day25.hs
+++
@@ -1,8 +0,0 @@
-module Day25 where
-
-import           AoC
-
-solve :: IO (Solution Int)
-solve = do
-  putStrLn "Workings of day 25 problems go here... 🚀"
-  pure $ NoSolution 2023 25
