be4b19054ee3590a28917853b7c6281291b13dc5
Marcin Wójcik <marcin@synthesis.co.za>
2023-12-02 01:36:19 +0200

    Refactor functions

--- marcin/powershell/day01/day01.ps1
+++ marcin/powershell/day01/day01.ps1
@@ -1,25 +1,24 @@
 param([string] $InputFile)
 
-function Solve-Day01-Part1 {
+function Get-Number($pattern, $numbermap) {
     cat $InputFile | % {
-        $matchlist = [regex]::Matches($_, "(\d)");
-        $first = ($matchlist | select -first 1).Groups[1].Value;
-        $last = ($matchlist | select -last 1).Groups[1].Value;
-        [long]"$first$last" 
-    } | measure -Sum | select -ExpandProperty Sum
-}
-
-function Solve-Day01-Part2 {
-    $numbers = @{ one = 1; two = 2; three = 3; four = 4; five = 5; six = 6; seven = 7; eight = 8; nine = 9; }
-    cat $InputFile | % {
-        $matchlist = [regex]::Matches($_, "(?=(\d|one|two|three|four|five|six|seven|eight|nine))");
+        $matchlist = [regex]::Matches($_, $pattern);
         $key1 = ($matchlist | select -first 1).Groups[1].Value;
         $key2 = ($matchlist | select -last 1).Groups[1].Value;
-        $first = $numbers[$key1] ? $numbers[$key1] : $key1;
-        $last = $numbers[$key2] ? $numbers[$key2] : $key2;
-        [long]"$first$last" 
+        $first = $numbermap[$key1] ? $numbermap[$key1] : $key1;
+        $last = $numbermap[$key2] ? $numbermap[$key2] : $key2;
+        [long]"$first$last"
     } | measure -Sum | select -ExpandProperty Sum
 }
 
-echo "Part 1: $(Solve-Day01-Part1)"
-echo "Part 2: $(Solve-Day01-Part2)"
+function Solve-Part1 {
+    Get-Number "(\d)" @{}
+}
+
+function Solve-Part2 {
+    $numbers = @{ one = 1; two = 2; three = 3; four = 4; five = 5; six = 6; seven = 7; eight = 8; nine = 9; }
+    Get-Number "(?=(\d|one|two|three|four|five|six|seven|eight|nine))" $numbers
+}
+
+echo "Part 1: $(Solve-Part1)"
+echo "Part 2: $(Solve-Part2)"
