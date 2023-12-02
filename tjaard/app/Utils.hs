c864899a436ac2bc9c8fdb326520dbcb26c1fab9
Tjaard du Plessis <tjaard@synthesis.co.za>
2023-12-02 17:45:30 +0200

    Refactor day 2 to use regex

--- tjaard/app/Utils.hs
+++ tjaard/app/Utils.hs
@@ -1,5 +1,6 @@
 module Utils where
 import Data.List (isInfixOf, isPrefixOf)
+import Text.Regex.PCRE
 
 halve :: String -> (String, String)
 halve s = splitAt (length s `div` 2) s
@@ -11,3 +12,10 @@ findSubstringIndex substring str
 
 startsWith :: String -> String -> Bool
 startsWith prefix str = prefix `isPrefixOf` str
+
+-- Haskell's way to get capture groups is ugly, I'm wrapping it here
+captureGroups :: String -> String -> [String]
+captureGroups str pattern =
+    captureGroups
+    where
+        (before, entireMatch, after, captureGroups) = str =~ pattern :: (String, String, String, [String])
