47d198a22d3b0639c719b1f5da752b8b5ccc0bd5
Marcin Wójcik <marcin@synthesis.co.za>
2023-12-03 16:30:18 +0200

    Refactor day03 again

--- marcin/powershell/day03/day03.ps1
+++ marcin/powershell/day03/day03.ps1
@@ -2,45 +2,28 @@ param([string] $InputFile)
 
 function Get-Parts {
     $lines = cat $InputFile
-    $(for ($i = 0; $i -lt $lines.Length; $i++) { 
-        Get-Part $i $($lines.Length -1) $($lines[0].Length -1)
-    })
-}
-
-function Get-Part($rownum, $lastrow, $lastcol) {
-    $above = [Math]::Max(0, $rownum - 1)
-    $below = [Math]::Min($lastrow, $rownum + 1)
-    $lines[$rownum] | sls "(\d+)" -AllMatches | % { $_.Matches | % { @{ Number = $_.Value; Index = $_.Index } } } | % {
-        $num = $_.Number
-        $leftcol = $_.Index
-        $rightcol = $leftcol + $num.Length - 1
-        $startcol = [Math]::Max(0, $leftcol - 1)
-        $endcol = [Math]::Min($lastcol, $rightcol + 1)
-        $ispart = $false
-        $gearpart = $false
-        $gearPositions = @()
-        for ($c = $startcol; $c -le $endcol; $c++) {
-            for ($r = $above; $r -le $below; $r++) {
-                $ispart = $ispart -or $(($lines[$r][$c] | sls "[\d\.]").Matches.Length -eq 0) 
-                if ($lines[$r][$c] -eq "*") {
-                    $gearpart = $true
-                    $gearPositions += "$r`:$c"
+    $lines | % {
+        $rownum = $lines.IndexOf($_);
+        $_ | sls "(\d+)" -AllMatches | % { $_.Matches | % { @{ Number = $_.Value; Index = $_.Index } } } | % {
+            $ispart = $false
+            $gearpart = $false
+            $links = @()
+            for ($col = [Math]::Max(0, $_.Index - 1); $col -le [Math]::Min($lines[0].Length -1, $_.Index + $_.Number.Length); $col++) {
+                for ($row = [Math]::Max(0, $rownum - 1); $row -le [Math]::Min($lines.Length -1, $rownum + 1); $row++) {
+                    $ispart = $ispart -or $(($lines[$row][$col] | sls "[\d\.]").Matches.Length -eq 0)
+                    $gearpart = $gearpart -or ($lines[$row][$col] -eq "*")
+                    $links += ($lines[$row][$col] -eq "*") ? "$row`:$col" : ""
                 }
             }
-        }
-        @{
-            Number = [int]$num;
-            IsPart = $ispart;
-            GearPart = $gearpart;
-            GearPositions = $gearPositions
-        }       
-    } | ? IsPart
+            @{ Number = $_.Number; IsPart = $ispart; GearPart = $gearpart; Links = $links }       
+        } | ? IsPart
+    }
 }
 
 function Solve-Part2 {  
     $gearParts = Get-Parts | ? GearPart
-    $gearlinks = $gearParts | select -ExpandProperty GearPositions | group | ? Count -eq 2 | select -ExpandProperty Name
-    $gearlinks | % {($gearParts | ? GearPositions -eq $_ | % { $_.Number }) -join "*" | iex } | measure -Sum | select -ExpandProperty Sum
+    $gearlinks = $gearParts | select -ExpandProperty Links | group | ? Count -eq 2 | select -ExpandProperty Name
+    $gearlinks | % {($gearParts | ? Links -eq $_ | % { $_.Number }) -join "*" | iex } | measure -Sum | select -ExpandProperty Sum
 }
 
 echo "Part 1: $(Get-Parts | select -ExpandProperty Number | measure -Sum | select -ExpandProperty Sum)"
