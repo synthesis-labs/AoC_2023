1dd7091c3ecb528ce201cce78076aa5d4efcff6c
Marcin Wójcik <marcin@synthesis.co.za>
2023-12-05 00:36:37 +0200

    Refactored day02 solution

--- marcin/powershell/day02/day02.ps1
+++ marcin/powershell/day02/day02.ps1
@@ -1,26 +1,22 @@
 param([string] $InputFile)
 
 $pieces = @{ red = 12; green = 13; blue = 14; }
-$cubes = cat $InputFile | % {
+$games = (cat $InputFile | sls "Game\s+(\d+):\s(.+)").Matches | % {
     $invalid = $false
     $mincolours = @{ red = 0; green = 0; blue = 0; }
-    $game = sls -InputObject $_ "Game\s(\d+):\s(.+)"
-    $turns = $game.Matches.Groups[2].Value -split '; '
-    $turns | % {
-        $turn = $_ | sls "(\d+)\s(red|green|blue)" -AllMatches
-        $turn.Matches | % {
-            $count = [int]$_.Groups[1].Value
-            $colour = $_.Groups[2].Value
-            $invalid = $invalid -or ($count -gt $pieces[$colour]) 
-            $mincolours[$colour] = [Math]::Max($mincolours[$colour], $count)   
-        }
+    $turns = $_.Groups[2].Value -split '\s*;\s*'
+    ($turns | sls "(\d+)\s(red|green|blue)" -AllMatches).Matches | % {
+        $cubes = [int]$_.Groups[1].Value
+        $colour = $_.Groups[2].Value
+        $invalid = $invalid -or ($cubes -gt $pieces[$colour]) 
+        $mincolours[$colour] = [Math]::Max($mincolours[$colour], $cubes)   
     }
     @{
-        Game = $game.Matches.Groups[1].Value;
+        Game = $_.Groups[1].Value;
         Valid = -not $invalid;
         Power = $mincolours["red"] * $mincolours["green"] * $mincolours["blue"]
     }
 }
 
-echo "Part 1: $( (($cubes | ? Valid).Game | measure -Sum).Sum )"
-echo "Part 2: $( (($cubes).Power | measure -Sum).Sum )"
+echo "Part 1: $( (($games | ? Valid).Game | measure -Sum).Sum )"
+echo "Part 2: $( (($games).Power | measure -Sum).Sum )"
