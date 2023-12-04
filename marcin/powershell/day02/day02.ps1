c2ff3ef6c45c3e099c1cffee4241542136bc91e5
Marcin Wójcik <marcin@synthesis.co.za>
2023-12-04 19:58:28 +0200

    Refactor day02 and day03 solutions

--- marcin/powershell/day02/day02.ps1
+++ marcin/powershell/day02/day02.ps1
@@ -1,28 +1,26 @@
 param([string] $InputFile)
 
-function Solve {
-    $pieces = @{ red = 12; green = 13; blue = 14; }
-    cat $InputFile | % {
-        $invalid = $false
-        $mincolours = @{ red = 0; green = 0; blue = 0; }
-        $game = sls -InputObject $_ "Game\s(\d+):\s(.+)"
-        $turns = $game.Matches.Groups[2].Value -split '; '
-        $turns | % {
-            $turn = $_ | sls "(\d+)\s(red|green|blue)" -AllMatches
-            $turn.Matches | % {
-                $cubes = [int]$_.Groups[1].Value
-                $colour = $_.Groups[2].Value
-                $invalid = $invalid -or ($cubes -gt $pieces[$colour]) 
-                $mincolours[$colour] = [Math]::Max($mincolours[$colour], $cubes)   
-            }
+$pieces = @{ red = 12; green = 13; blue = 14; }
+$cubes = cat $InputFile | % {
+    $invalid = $false
+    $mincolours = @{ red = 0; green = 0; blue = 0; }
+    $game = sls -InputObject $_ "Game\s(\d+):\s(.+)"
+    $turns = $game.Matches.Groups[2].Value -split '; '
+    $turns | % {
+        $turn = $_ | sls "(\d+)\s(red|green|blue)" -AllMatches
+        $turn.Matches | % {
+            $count = [int]$_.Groups[1].Value
+            $colour = $_.Groups[2].Value
+            $invalid = $invalid -or ($count -gt $pieces[$colour]) 
+            $mincolours[$colour] = [Math]::Max($mincolours[$colour], $count)   
         }
-        @{
-            Game = $game.Matches.Groups[1].Value;
-            Valid = -not $invalid;
-            Power = $mincolours["red"] * $mincolours["green"] * $mincolours["blue"]
-        }
+    }
+    @{
+        Game = $game.Matches.Groups[1].Value;
+        Valid = -not $invalid;
+        Power = $mincolours["red"] * $mincolours["green"] * $mincolours["blue"]
     }
 }
 
-echo "Part 1: $(Solve | ? Valid | select -ExpandProperty Game | measure -Sum | select -ExpandProperty Sum )"
-echo "Part 2: $(Solve | select -ExpandProperty Power | measure -Sum | select -ExpandProperty Sum)"
+echo "Part 1: $($cubes | ? Valid | select -ExpandProperty Game | measure -Sum | select -ExpandProperty Sum )"
+echo "Part 2: $($cubes | select -ExpandProperty Power | measure -Sum | select -ExpandProperty Sum)"
