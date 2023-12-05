87101b42f12b51408963e396c3d6e82f1fa93646
Tom Wells <tom@synthesis.co.za>
2023-12-05 21:41:56 +0100

    day 5 cleanup

--- tom/app/Day05.hs
+++ tom/app/Day05.hs
@@ -23,25 +23,25 @@ data Plan =
   Plan [Int] [MapArrow]
 
 parse_range :: Parsec String () (Int, Int, Int)
-parse_range = do
-  dest_start <- read <$> many1 digit <* char ' '
-  source_start <- read <$> many1 digit <* char ' '
-  range_length <- read <$> many1 digit
-  pure (dest_start, source_start, range_length)
+parse_range =
+  let dest_start = read <$> many1 digit <* char ' '
+      source_start = read <$> many1 digit <* char ' '
+      range_length = read <$> many1 digit
+   in do (,,) <$> dest_start <*> source_start <*> range_length
 
 parse_map :: Parsec String () MapArrow
-parse_map = do
-  from <- many1 letter <* string "-to-"
-  to <- many1 letter <* string " map:" <* newline
-  ranges <- many1 (parse_range <* newline) <* optional newline
-  pure $ MapArrow from to (mk_mapper ranges)
+parse_map =
+  let from = many1 letter <* string "-to-"
+      to = many1 letter <* string " map:" <* newline
+      ranges = many1 (parse_range <* newline) <* optional newline
+   in do MapArrow <$> from <*> to <*> (mk_mapper <$> ranges)
 
 parse_plan :: Parsec String () Plan
-parse_plan = do
-  seeds <-
-    string "seeds: " *> (read <$> many1 digit) `sepBy` char ' ' <* newline
-  mappers <- newline *> many1 parse_map
-  pure $ Plan seeds mappers
+parse_plan =
+  let seeds =
+        string "seeds: " *> (read <$> many1 digit) `sepBy` char ' ' <* newline
+      mappers = newline *> many1 parse_map
+   in do Plan <$> seeds <*> mappers
 
 source_within_range :: Int -> (Int, Int, Int) -> Bool
 source_within_range source (dst, src, len) = source >= src && source < src + len
@@ -55,6 +55,7 @@ mk_mapper ranges input =
 solve1 :: Plan -> Int
 solve1 (Plan seeds arrows) =
   let mapper = foldl (\acc -> \(MapArrow f t m) -> m . acc) id arrows
+   -- ^^ Foldl a composition of the arrows together to make an uber mapper
    in minimum $ mapper <$> seeds
 
 seeds_as_ranges :: [Int] -> [(Int, Int)]
@@ -65,7 +66,7 @@ solve2 :: Plan -> Int
 solve2 (Plan seeds arrows) =
   let mapper = foldl (\acc -> \(MapArrow f t m) -> m . acc) id arrows
       seed_ranges = seeds_as_ranges seeds
-      checker (i1, v1) (i2, v2) =
+      search_function (i1, v1) (i2, v2) =
         if (i2 - i1) == (v2 - v1)
           then Skip
           else if abs (i2 - i1) == 1
@@ -73,33 +74,33 @@ solve2 (Plan seeds arrows) =
                  else Continue
       results =
         join $
-        (\(from, len) -> seeker (from) (from + len + 1) mapper checker) <$>
+        (\(from, len) -> seeker (from) (from + len) mapper search_function) <$>
         seed_ranges
    in minimum $ snd <$> results
 
-data SeekResult
-  = Skip
-  | Continue
-  | Found (Int, Int)
+data SeekResult a
+  = Skip -- Region is continuous, so skip it
+  | Continue -- Region is non-continuous, so keep searching
+  | Found (Int, a) -- Found the spot!
 
 -- A magical binary search that will search the entire space looking for
 -- interesting features (such as jumps in continuous space)
 seeker ::
      Int
   -> Int
-  -> (Int -> Int)
-  -> ((Int, Int) -> (Int, Int) -> SeekResult)
-  -> [(Int, Int)]
+  -> (Int -> a)
+  -> ((Int, a) -> (Int, a) -> SeekResult a)
+  -> [(Int, a)]
 seeker start stop produce continuous =
-  let startv = produce start
-      stopv = produce stop
+  let (startv, stopv) = (produce start, produce stop)
    in case continuous (start, startv) (stop, stopv) of
         Skip -> []
         Found v -> [v]
         Continue ->
           let mid = (start + stop) `div` 2
+           -- Split the region and fork the search
            in seeker start mid produce continuous ++
-              seeker (mid + 1) stop produce continuous
+              seeker mid stop produce continuous
 
 solve :: IO (Solution Int)
 solve = do
