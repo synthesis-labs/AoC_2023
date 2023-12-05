c8bb35134a80defc26d35034abdfdb1a7ad0859b
marinda_rossouw <Marinda@synthesis.co.za>
2023-12-05 10:55:02 +0200

    Day 4 Part 1

--- marinda/src/Tests/Advent_Day4_Scratchcards_Tests.java
+++ marinda/src/Tests/Advent_Day4_Scratchcards_Tests.java
@@ -1,114 +1,143 @@
 package Tests;
 
 import AdventCode.Advent_Day3_GearRatios;
+import AdventCode.Advent_Day4_Scratchcards;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class Advent_Day4_Scratchcards_Tests {
 
     /**
-     * --- Day 3: Gear Ratios ---
+     * --- Day 4: Scratchcards ---
      * <p>
-     * You and the Elf eventually reach a gondola lift station; he says the gondola lift will take you up to the water source, but this is as far as he can bring you. You go inside.
+     * The gondola takes you up. Strangely, though, the ground doesn't seem to be coming with you; you're not climbing a mountain. As the circle of Snow Island recedes below
+     * you, an entire new landmass suddenly appears above you! The gondola carries you to the surface of the new island and lurches into the station.
      * <p>
-     * It doesn't take long to find the gondolas, but there seems to be a problem: they're not moving.
+     * As you exit the gondola, the first thing you notice is that the air here is much warmer than it was on Snow Island. It's also quite humid. Is this where the water source is?
      * <p>
-     * "Aaah!"
+     * The next thing you notice is an Elf sitting on the floor across the station in what seems to be a pile of colorful square cards.
      * <p>
-     * You turn around to see a slightly-greasy Elf with a wrench and a look of surprise. "Sorry, I wasn't expecting anyone! The gondola lift isn't working right now; it'll still be a while before I can fix it." You offer to help.
+     * "Oh! Hello!" The Elf excitedly runs over to you. "How may I be of service?" You ask about water sources.
      * <p>
-     * The engineer explains that an engine part seems to be missing from the engine, but nobody can figure out which one. If you can add up all the part numbers in the engine schematic, it should be easy to work out which part is missing.
+     * "I'm not sure; I just operate the gondola lift. That does sound like something we'd have, though - this is Island Island, after all! I bet the gardener would know. He's
+     * on a different island, though - er, the small kind surrounded by water, not the floating kind. We really need to come up with a better naming scheme. Tell you what: if
+     * you can help me with something quick, I'll let you borrow my boat and you can go visit the gardener. I got all these scratchcards as a gift, but I can't figure out what
+     * I've won."
      * <p>
-     * The engine schematic (your puzzle input) consists of a visual representation of the engine. There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. (Periods (.) do not count as a symbol.)
+     * The Elf leads you over to the pile of colorful cards. There, you discover dozens of scratchcards, all with their opaque covering already scratched off. Picking one up, it
+     * looks like each card has two lists of numbers separated by a vertical bar (|): a list of winning numbers and then a list of numbers you have. You organize the information
+     * into a table (your puzzle input).
      * <p>
-     * Here is an example engine schematic:
+     * As far as the Elf has been able to figure out, you have to figure out which of the numbers you have appear in the list of winning numbers. The first match makes the card
+     * worth one point and each match after the first doubles the point value of that card.
      * <p>
-     * 467..114..
-     * ...*......
-     * ..35..633.
-     * ......#...
-     * 617*......
-     * .....+.58.
-     * ..592.....
-     * ......755.
-     * ...$.*....
-     * .664.598..
+     * For example:
      * <p>
-     * In this schematic, two numbers are not part numbers because they are not adjacent to a symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol and so is a part number; their sum is 4361.
+     * Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+     * Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+     * Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+     * Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+     * Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+     * Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
      * <p>
-     * Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
+     * In the above example, card 1 has five winning numbers (41, 48, 83, 86, and 17) and eight numbers you have (83, 86, 6, 31, 17, 9, 48, and 53). Of the numbers you have,
+     * four of them (48, 83, 17, and 86) are winning numbers! That means card 1 is worth 8 points (1 for the first match, then doubled three times for each of the three matches
+     * after the first).
+     * <p>
+     * Card 2 has two winning numbers (32 and 61), so it is worth 2 points.
+     * Card 3 has two winning numbers (1 and 21), so it is worth 2 points.
+     * Card 4 has one winning number (84), so it is worth 1 point.
+     * Card 5 has no winning numbers, so it is worth no points.
+     * Card 6 has no winning numbers, so it is worth no points.
+     * <p>
+     * So, in this example, the Elf's pile of scratchcards is worth 13 points.
+     * <p>
+     * Take a seat in the large pile of colorful cards. How many points are they worth in total?
      */
     //region Part 1:
     @Test
     public void Part1_Test_SampleData() {
 
-        Advent_Day3_GearRatios advent_Day3 = new Advent_Day3_GearRatios();
+        Advent_Day4_Scratchcards advent_Day4 = new Advent_Day4_Scratchcards();
+
+        List<Integer> ourWinningNumbers = advent_Day4.findOurWinningNumbers(List.of(12, 5, 1, 6), List.of(3, 6, 2, 1, 10, 12));
+
+        assertEquals("[6, 1, 12]", ourWinningNumbers.toString());
+
 
         ArrayList<String> sampleData = new ArrayList<>();
-        sampleData.add("467..114..");
-        sampleData.add("...*......");
-        sampleData.add("..35..633.");
-        sampleData.add("......#...");
-        sampleData.add("617*......");
-        sampleData.add(".....+.58.");
-        sampleData.add("..592.....");
-        sampleData.add("......755.");
-        sampleData.add("...$.*....");
-        sampleData.add(".664.598..");
+        sampleData.add("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
+        sampleData.add("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19");
+        sampleData.add("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1");
+        sampleData.add("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83");
+        sampleData.add("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36");
+        sampleData.add("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11");
 
+        HashMap<String, List<Integer>> scratchcardsWinningNumbers = advent_Day4.findWinningNumbersPerCard(sampleData);
 
-        int partNumbersSum = advent_Day3.Part1(sampleData);
+        assertEquals("{1=[83, 86, 17, 48], 2=[61, 32], 3=[21, 1], 4=[84]}", scratchcardsWinningNumbers.toString());
 
-        assertEquals(4361, partNumbersSum);
+        int scratchcardsPoints = advent_Day4.Part1(sampleData);
+
+        assertEquals(13, scratchcardsPoints);
     }
 
     @Test
     public void Part1_Test_RealData() throws IOException {
-        Advent_Day3_GearRatios Advent_Day3 = new Advent_Day3_GearRatios();
+        Advent_Day4_Scratchcards advent_Day4 = new Advent_Day4_Scratchcards();
 
-        ArrayList<String> data = (ArrayList<String>) Files.readAllLines(Paths.get("./src/Data/Advent_Day3_GearRatios_Data.txt"));
+        ArrayList<String> data = (ArrayList<String>) Files.readAllLines(Paths.get("./src/Data/Advent_Day4_Scratchcards_Data.txt"));
 
-        int partNumbersSum = Advent_Day3.Part1(data);
+        int scratchcardsPoints = advent_Day4.Part1(data);
 
-        assertEquals(539713, partNumbersSum);
+        assertEquals(13, scratchcardsPoints);
     }
     //endregion
 
     /**
      * --- Part Two ---
      * <p>
-     * The engineer finds the missing part and installs it in the engine! As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+     * Just as you're about to report your findings to the Elf, one of you realizes that the rules have actually been printed on the back of every card this whole time.
      * <p>
-     * You don't seem to be going very fast, though. Maybe something is still wrong? Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+     * There's no such thing as "points". Instead, scratchcards only cause you to win more scratchcards equal to the number of winning numbers you have.
      * <p>
-     * Before you can explain the situation, she suggests that you look out the window. There stands the engineer, holding a phone in one hand and waving with the other. You're going so slowly that you haven't even left the station. You exit the gondola.
+     * Specifically, you win copies of the scratchcards below the winning card equal to the number of matches. So, if card 10 were to have 5 matching numbers, you would win one
+     * copy each of cards 11, 12, 13, 14, and 15.
      * <p>
-     * The missing part wasn't the only issue - one of the gears in the engine is wrong. A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+     * Copies of scratchcards are scored like normal scratchcards and have the same card number as the card they copied. So, if you win a copy of card 10 and it has 5 matching
+     * numbers, it would then win a copy of the same cards that the original card 10 won: cards 11, 12, 13, 14, and 15. This process repeats until none of the copies cause you
+     * to win any more cards. (Cards will never make you copy a card past the end of the table.)
      * <p>
-     * This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced.
+     * This time, the above example goes differently:
      * <p>
-     * Consider the same engine schematic again:
+     * Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+     * Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+     * Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+     * Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+     * Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+     * Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
      * <p>
-     * 467..114..
-     * ...*......
-     * ..35..633.
-     * ......#...
-     * 617*......
-     * .....+.58.
-     * ..592.....
-     * ......755.
-     * ...$.*....
-     * .664.598..
+     * Card 1 has four matching numbers, so you win one copy each of the next four cards: cards 2, 3, 4, and 5.
+     * Your original card 2 has two matching numbers, so you win one copy each of cards 3 and 4.
+     * Your copy of card 2 also wins one copy each of cards 3 and 4.
+     * Your four instances of card 3 (one original and three copies) have two matching numbers, so you win four copies each of cards 4 and 5.
+     * Your eight instances of card 4 (one original and seven copies) have one matching number, so you win eight copies of card 5.
+     * Your fourteen instances of card 5 (one original and thirteen copies) have no matching numbers and win no more cards.
+     * Your one instance of card 6 (one original) has no matching numbers and wins no more cards.
      * <p>
-     * In this schematic, there are two gears. The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345. The second gear is in the lower right; its gear ratio is 451490. (The * adjacent to 617 is not a gear because it is only adjacent to one part number.) Adding up all of the gear ratios produces 467835.
+     * Once all of the originals and copies have been processed, you end up with 1 instance of card 1, 2 instances of card 2, 4 instances of card 3, 8 instances of card 4, 14
+     * instances of card 5, and 1 instance of card 6. In total, this example pile of scratchcards causes you to ultimately have 30 scratchcards!
      * <p>
-     * What is the sum of all of the gear ratios in your engine schematic?
+     * Process all of the original and copied scratchcards until no more scratchcards are won. Including the original set of scratchcards, how many total scratchcards do you end
+     * up with?
      */
     //region Part 2:
     @Test
