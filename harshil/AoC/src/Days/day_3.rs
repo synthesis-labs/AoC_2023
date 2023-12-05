6d77a85fe80345a91863dc7b6b071be5fb92d900
TelaTotiusTerrae <harshilsheganlall1304@gmail.com>
2023-12-05 19:56:09 +0200

    day 3 pt 1 IP

--- harshil/AoC/src/Days/day_3.rs
+++ harshil/AoC/src/Days/day_3.rs
@@ -6,5 +6,25 @@ pub fn day_2_pt1() {
     let mut contents = String::new();
     file.unwrap().read_to_string(&mut contents).expect("Error reading file");
 
+    let mut vcontent: Vec<String>= contents.split("\n").collect();
+    
+}
 
+pub  fn check_for_symbol(line:String)->bool{
+    
+}
+
+pub fn get_Index_number(line:String,indicies: Vec<i32>){
+    
+}
+
+pub fn get_number(line:String) -> Vec<i32>{
+    let mut numbers:Vec<i32> = vec![];
+    let mut tnum:String;
+    for (index, char) in line.char_indices() {
+        if line.chars().nth(index).unwrap().is_digit(10) {
+            tnum.push(line.chars().nth(index).and_then(|x| x.to_digit(10)).map(|d| d as char).unwrap_or('0'))
+        }
+    }
+    return numbers
 }
