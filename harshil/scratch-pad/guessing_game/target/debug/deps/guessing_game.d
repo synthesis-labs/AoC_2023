3f1aab5264a3aafc29b9aa8f68b55534b71baabb
HarshilSheganlall <Harshil@synthesis.co.za>
2023-12-06 13:23:22 +0200

    removed target folders

--- harshil/scratch-pad/guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-C:\Users\harsh\RustroverProjects\AoC_2023\harshil\scratch-pad\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-C:\Users\harsh\RustroverProjects\AoC_2023\harshil\scratch-pad\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-src\main.rs:
