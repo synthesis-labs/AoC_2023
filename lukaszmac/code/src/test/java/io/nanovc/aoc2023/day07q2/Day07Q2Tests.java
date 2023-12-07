2c0850a246ff7ac6bdd3411645a1567a9ebf930d
Luke Machowski <luke@synthesis.co.za>
2023-12-07 15:52:05 +0200

    Got 2023 Day 07 Q2.

--- lukaszmac/code/src/test/java/io/nanovc/aoc2023/day07q2/Day07Q2Tests.java
+++ lukaszmac/code/src/test/java/io/nanovc/aoc2023/day07q2/Day07Q2Tests.java
@@ -1,73 +1,41 @@
-package io.nanovc.aoc2023.day07q1;
+package io.nanovc.aoc2023.day07q2;
 
 import io.nanovc.aoc2023.TestBase;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
 
-import static io.nanovc.aoc2023.day07q1.Day07Q1Tests.Solution1Tests.Card.*;
-import static io.nanovc.aoc2023.day07q1.Day07Q1Tests.Solution1Tests.HandType.*;
+import static io.nanovc.aoc2023.day07q2.Day07Q2Tests.Solution1Tests.Card.*;
+import static io.nanovc.aoc2023.day07q2.Day07Q2Tests.Solution1Tests.HandType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * --- Day 7: Camel Cards ---
- * Your all-expenses-paid trip turns out to be a one-way, five-minute ride in an airship. (At least it's a cool airship!) It drops you off at the edge of a vast desert and descends back to Island Island.
+ * --- Part Two ---
+ * To make things a little more interesting, the Elf introduces one additional rule. Now, J cards are jokers - wildcards that can act like whatever card would make the hand the strongest type possible.
  *
- * "Did you bring the parts?"
+ * To balance this, J cards are now the weakest individual cards, weaker even than 2. The other cards stay in the same order: A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J.
  *
- * You turn around to see an Elf completely covered in white clothing, wearing goggles, and riding a large camel.
+ * J cards can pretend to be whatever card is best for the purpose of determining hand type; for example, QJJQ2 is now considered four of a kind. However, for the purpose of breaking ties between two hands of the same type, J is always treated as J, not the card it's pretending to be: JKKK2 is weaker than QQQQ2 because J is weaker than Q.
  *
- * "Did you bring the parts?" she asks again, louder this time. You aren't sure what parts she's looking for; you're here to figure out why the sand stopped.
- *
- * "The parts! For the sand, yes! Come with me; I will show you." She beckons you onto the camel.
- *
- * After riding a bit across the sands of Desert Island, you can see what look like very large rocks covering half of the horizon. The Elf explains that the rocks are all along the part of Desert Island that is directly above Island Island, making it hard to even get there. Normally, they use big machines to move the rocks and filter the sand, but the machines have broken down because Desert Island recently stopped receiving the parts they need to fix the machines.
- *
- * You've already assumed it'll be your job to figure out why the parts stopped when she asks if you can help. You agree automatically.
- *
- * Because the journey will take a few days, she offers to teach you the game of Camel Cards. Camel Cards is sort of similar to poker except it's designed to be easier to play while riding a camel.
- *
- * In Camel Cards, you get a list of hands, and your goal is to order them based on the strength of each hand. A hand consists of five cards labeled one of A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2. The relative strength of each card follows this order, where A is the highest and 2 is the lowest.
- *
- * Every hand is exactly one type. From strongest to weakest, they are:
- *
- * Five of a kind, where all five cards have the same label: AAAAA
- * Four of a kind, where four cards have the same label and one card has a different label: AA8AA
- * Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
- * Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
- * Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
- * One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
- * High card, where all cards' labels are distinct: 23456
- * Hands are primarily ordered based on type; for example, every full house is stronger than any three of a kind.
- *
- * If two hands have the same type, a second ordering rule takes effect. Start by comparing the first card in each hand. If these cards are different, the hand with the stronger first card is considered stronger. If the first card in each hand have the same label, however, then move on to considering the second card in each hand. If they differ, the hand with the higher second card wins; otherwise, continue with the third card in each hand, then the fourth, then the fifth.
- *
- * So, 33332 and 2AAAA are both four of a kind hands, but 33332 is stronger because its first card is stronger. Similarly, 77888 and 77788 are both a full house, but 77888 is stronger because its third card is stronger (and both hands have the same first and second card).
- *
- * To play Camel Cards, you are given a list of hands and their corresponding bid (your puzzle input). For example:
+ * Now, the above example goes very differently:
  *
  * 32T3K 765
  * T55J5 684
  * KK677 28
  * KTJJT 220
  * QQQJA 483
- * This example shows five hands; each hand is followed by its bid amount. Each hand wins an amount equal to its bid multiplied by its rank, where the weakest hand gets rank 1, the second-weakest hand gets rank 2, and so on up to the strongest hand. Because there are five hands in this example, the strongest hand will have rank 5 and its bid will be multiplied by 5.
+ * 32T3K is still the only one pair; it doesn't contain any jokers, so its strength doesn't increase.
+ * KK677 is now the only two pair, making it the second-weakest hand.
+ * T55J5, KTJJT, and QQQJA are now all four of a kind! T55J5 gets rank 3, QQQJA gets rank 4, and KTJJT gets rank 5.
+ * With the new joker rule, the total winnings in this example are 5905.
  *
- * So, the first step is to put the hands in order of strength:
- *
- * 32T3K is the only one pair and the other hands are all a stronger type, so it gets rank 1.
- * KK677 and KTJJT are both two pair. Their first cards both have the same label, but the second card of KK677 is stronger (K vs T), so KTJJT gets rank 2 and KK677 gets rank 3.
- * T55J5 and QQQJA are both three of a kind. QQQJA has a stronger first card, so it gets rank 5 and T55J5 gets rank 4.
- * Now, you can determine the total winnings of this set of hands by adding up the result of multiplying each hand's bid with its rank (765 * 1 + 220 * 2 + 28 * 3 + 684 * 4 + 483 * 5). So the total winnings in this example are 6440.
- *
- * Find the rank of every hand in your set. What are the total winnings?
+ * Using the new joker rule, find the rank of every hand in your set. What are the new total winnings?
  * <p>
  * Website:
- * <a href="https://adventofcode.com/2023/day/7">Challenge</a>
+ * <a href="https://adventofcode.com/2023/day/7#part2">Challenge</a>
  */
-public abstract class Day07Q1Tests extends TestBase
+public abstract class Day07Q2Tests extends TestBase
 {
 
     /**
@@ -79,7 +47,7 @@ public abstract class Day07Q1Tests extends TestBase
     @Override
     protected String getDayLabel()
     {
-        return "Day 07 Q1";
+        return "Day 07 Q2";
     }
 
     /**
@@ -107,7 +75,7 @@ public abstract class Day07Q1Tests extends TestBase
     @Override
     protected String getSampleAnswer()
     {
-        return "6440";
+        return "5905";
     }
 
     /**
@@ -143,10 +111,10 @@ public abstract class Day07Q1Tests extends TestBase
     @Override
     protected String getActualAnswer()
     {
-        return "248569531";
+        return "250382098";
     }
 
-    public static class Solution1Tests extends Day07Q1Tests
+    public static class Solution1Tests extends Day07Q2Tests
     {
 
         /**
@@ -226,16 +194,16 @@ public abstract class Day07Q1Tests extends TestBase
 
         public enum Card
         {
-            _2("2"   , 2  ),
-            _3("3"   , 3  ),
-            _4("4"   , 4  ),
-            _5("5"   , 5  ),
-            _6("6"   , 6  ),
-            _7("7"   , 7  ),
-            _8("8"   , 8  ),
-            _9("9"   , 9  ),
+            _J("Joker", 1  ),
+            _2("2"    , 2  ),
+            _3("3"    , 3  ),
+            _4("4"    , 4  ),
+            _5("5"    , 5  ),
+            _6("6"    , 6  ),
+            _7("7"    , 7  ),
+            _8("8"    , 8  ),
+            _9("9"    , 9  ),
             _T("Ten"  , 10 ),
-            _J("Jack" , 11 ),
             _Q("Queen", 12 ),
             _K("King" , 13 ),
             _A("Ace"  , 14 );
@@ -299,6 +267,8 @@ public abstract class Day07Q1Tests extends TestBase
                 indexCards(cards, cardToCountMap, countToCardMap);
                 // Now we have the card counts.
 
+                // Check whether we have jokers:
+                Integer jokerCount = cardToCountMap.getOrDefault(_J, 0);
 
                 // Figure out the hand type based on what we see:
                 return switch (cardSet.size())
@@ -311,8 +281,21 @@ public abstract class Day07Q1Tests extends TestBase
 
                         yield switch (highestEntry.getKey())
                         {
-                            case 4 -> FourOfAKind;
-                            case 3 -> FullHouse;
+                            case 4 -> {
+                                yield switch (jokerCount)
+                                {
+                                    case 1, 2, 3, 4, 5 -> FiveOfAKind;
+                                    default -> FourOfAKind;
+                                };
+                            }
+                            case 3 -> {
+                                yield switch (jokerCount)
+                                {
+                                    case 1 -> FourOfAKind;
+                                    case 2, 3, 4, 5 -> FiveOfAKind;
+                                    default -> FullHouse;
+                                };
+                            }
                             default -> Normal;
                         };
                     }
@@ -323,8 +306,25 @@ public abstract class Day07Q1Tests extends TestBase
 
                         yield switch (highestEntry.getKey())
                         {
-                            case 2 -> TwoPair;
-                            case 3 -> ThreeOfAKind;
+                            case 2 ->
+                            {
+                                yield switch (jokerCount)
+                                {
+                                    case 1 -> FullHouse;
+                                    case 2 -> FourOfAKind;
+                                    case 3 -> FiveOfAKind;
+                                    default -> TwoPair;
+                                };
+                            }
+                            case 3 -> {
+                                yield switch (jokerCount)
+                                {
+                                    case 1 -> FourOfAKind;
+                                    case 2 -> FiveOfAKind;
+                                    case 3 -> FourOfAKind;
+                                    default -> ThreeOfAKind;
+                                };
+                            }
                             default -> Normal;
                         };
                     }
@@ -335,12 +335,28 @@ public abstract class Day07Q1Tests extends TestBase
 
                         yield switch (highestEntry.getKey())
                         {
-                            case 2 -> OnePair;
+                            case 2 ->{
+                                yield switch (jokerCount)
+                                {
+                                    case 1, 2 -> ThreeOfAKind;
+                                    case 3, 4, 5 -> FiveOfAKind;
+                                    default -> OnePair;
+                                };
+                            }
                             default -> Normal;
                         };
                     }
 
-                    case 5 -> HighCard;
+                    case 5 -> {
+                        yield switch (jokerCount)
+                        {
+                            case 1 -> OnePair;
+                            case 2 -> ThreeOfAKind;
+                            case 3 -> FourOfAKind;
+                            case 4, 5 -> FiveOfAKind;
+                            default -> HighCard;
+                        };
+                    }
 
                     default -> Normal;
                 };
@@ -409,19 +425,19 @@ public abstract class Day07Q1Tests extends TestBase
         }
 
         @Test
-        public void testHandType_T55J5_ThreeOfAKind()
+        public void testHandType_T55J5_FourOfAKind()
         {
             var cards = List.of(_T, _5, _5, _J, _5);
             var handType = HandType.rateHandType(cards);
-            assertEquals(ThreeOfAKind, handType);
+            assertEquals(FourOfAKind, handType);
         }
 
         @Test
-        public void testHandType_QQQQJA_ThreeOfAKind()
+        public void testHandType_QQQJA_FourOfAKind()
         {
             var cards = List.of(_Q, _Q, _Q, _J, _A);
             var handType = HandType.rateHandType(cards);
-            assertEquals(ThreeOfAKind, handType);
+            assertEquals(FourOfAKind, handType);
         }
 
         @Test
@@ -433,11 +449,11 @@ public abstract class Day07Q1Tests extends TestBase
         }
 
         @Test
-        public void testHandType_KTJJT7_TwoPair()
+        public void testHandType_KTJJT_FourOfAKind()
         {
             var cards = List.of(_K, _T, _J, _J, _T);
             var handType = HandType.rateHandType(cards);
-            assertEquals(TwoPair, handType);
+            assertEquals(FourOfAKind, handType);
         }
 
         @Test
@@ -496,15 +512,93 @@ public abstract class Day07Q1Tests extends TestBase
             assertEquals(HighCard, handType);
         }
 
-        /*
-            HighCard,
-            OnePair,
-            TwoPair,
-            ThreeOfAKind,
-            FullHouse,
-            FourOfAKind,
-            FiveOfAKind;
-         */
+        @Test
+        public void testHandType_QJJQ2_FourOfAKind()
+        {
+            var cards = List.of(_Q, _J, _J, _Q, _2);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(FourOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_JKKK2_FourOfAKind()
+        {
+            var cards = List.of(_J, _K, _K, _K, _2);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(FourOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_QQQQ2_FourOfAKind()
+        {
+            var cards = List.of(_Q, _Q, _Q, _Q, _2);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(FourOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_22JJJ_FiveOfAKind()
+        {
+            var cards = List.of(_2, _2, _J, _J, _J);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(FiveOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_JJ234_ThreeOfAKind()
+        {
+            var cards = List.of(_J, _J, _2, _3, _4);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(ThreeOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_AAKKJ_FullHouse()
+        {
+            var cards = List.of(_A, _A, _K, _K, _J);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(FullHouse, handType);
+        }
+
+        @Test
+        public void testHandType_234JJ_ThreeOfAKind()
+        {
+            var cards = List.of(_2, _3, _4, _J, _J);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(ThreeOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_2344J_ThreeOfAKind()
+        {
+            var cards = List.of(_2, _3, _4, _4, _J);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(ThreeOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_23444_ThreeOfAKind()
+        {
+            var cards = List.of(_2, _3, _4, _4, _4);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(ThreeOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_JJJJJ_FiveOfAKind()
+        {
+            var cards = List.of(_J, _J, _J, _J, _J);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(FiveOfAKind, handType);
+        }
+
+        @Test
+        public void testHandType_23JJJ_FourOfAKind()
+        {
+            var cards = List.of(_2, _3, _J, _J, _J);
+            var handType = HandType.rateHandType(cards);
+            assertEquals(FourOfAKind, handType);
+        }
     }
 
 }
