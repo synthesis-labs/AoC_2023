39a6fed65c85868b1b4831a9ac82da02d031da17
Marais Neethling <marais@synthesis.co.za>
2023-12-12 15:48:06 +0200

    Marais - Day 12 p2 not working

--- marais/12/solve.py
+++ marais/12/solve.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 
 
 def read_input(file='./test.txt'):
@@ -24,7 +25,7 @@ def parse(lines):
     return parsed
 
 
-def solve1(line):
+def solve1(line) -> list[int]:
     #    ???.### 1,1,3
     # this is binary where . is 0 and # is 1
     exp = len(line['s'])
@@ -36,7 +37,7 @@ def solve1(line):
     # print(mask_s)
     # print(bin(mask))
     candidates = []
-    sol_count = 0
+    solutions = []
     for i in range(2**exp):
         can = (i ^ bin_s) & mask
         # can = can_int & mask
@@ -56,18 +57,53 @@ def solve1(line):
         # print(groups)
         # print(line['g'])
         if groups == line['g']:
-            # print(f"found {candidate:0{exp}b}")
-            sol_count += 1
+            print(f"found {candidate:0{exp}b}")
+            solutions.append(candidate)
 
-    print(f"{line['s']} -> {sol_count}")
-    return sol_count
+    # print(f"{line['s']} -> {len(solutions)}")
+    return solutions
 
 
 def expand(lines):
     for line in lines:
-        line['s'] = '?'.join(line['s'] for i in range(5))
-        line['g'] = line['g'] * 5
+        join_char = '.' if line['s'].endswith('#') else '?'
+        extra_char = '?' if line['s'].startswith('?') and line['s'].endswith('?') else ''
+        line['s'] = [line['s']+extra_char] + [join_char+line['s']+extra_char for i in range(4)]
+        line['g'] = [line['g'] for i in range(5)]
     return lines
+
+
+def solve2(line):
+    print(f"{line['s']} -> {line['g']}")
+    if line['s'].startswith('?') and line['s'].endswith('?'):
+        # get solutions so we can reason further
+        solutions = solve1({'s': line['s']+'?', 'g': line['g']})
+        exp = len(line['s']) + 1
+        # if all solutions ends with a 1, the join char should be a .(0) and added to the beginning
+        left_char = '.' if all([bin(s).endswith('1') for s in solutions]) else '?' if all([bin(s).endswith('0') for s in solutions]) else ''
+        right_char = '' if left_char else '.' if all([f"{s:0{exp}b}".startswith('1') for s in solutions]) else '?' if all([f"{s:0{exp}b}".startswith('0') for s in solutions]) else ''
+
+        if left_char == '' and right_char == '':
+            raise Exception("I don't know what to do")
+
+    else:
+        # if line['s'] ends in #(1), the join char should be a .(0) and added to the beginning
+        # if line['s'] ends in .(0), the join char should be a ? (1 or 0) and should be added to the beginning
+        left_char = '.' if line['s'].endswith('#') else '?' if line['s'].endswith('.') else ''
+        # if line['s'] starts with a #(1), the join char should be a .(0) and added to the end
+        # if line['s'] starts with a .(0), the join char should be a ? (1 or 0) and should be added to the end
+        right_char = '' if left_char else '.' if line['s'].startswith('#') else '?' if line['s'].startswith('.') else ''
+
+        if left_char == '' and right_char == '':
+            raise Exception("I don't know what to do")
+
+    print(f"{line['s']} -> {left_char}*{line['s']}*{right_char}")
+
+    expanded = [line['s'] + right_char] + [left_char + line['s'] + right_char for i in range(3)] + [left_char + line['s']]
+    # line['g'] = [line['g'] for i in range(5)]
+    solutions = [len(solve1({'s': s, 'g': line['g']})) for s in expanded]
+    # print(f"{line['s']} expanded -> {solutions}")
+    return math.prod(solutions)
 
 
 if __name__ == '__main__':
@@ -76,10 +112,10 @@ if __name__ == '__main__':
 
     lines = read_input('test.txt')
     lines = parse(lines)
-    answ1 = sum([solve1(line) for line in lines])
-    print(f"Answer 1: {answ1}")
+    # answ1 = sum([len(solve1(line)) for line in lines])
+    # print(f"Answer 1: {answ1}\n\n")
 
-    lines = expand(lines)
+    # lines = expand(lines)
     # print(lines)
-    answ2 = sum([solve1(line) for line in lines])
-    print(f"Answer 2: {answ2}")
+    answ2 = [solve2(line) for line in lines]
+    print(f"Answer 2: {sum(answ2)}")
--- micky_gee/day_11/process.py
+++ micky_gee/day_11/process.py
@@ -19,10 +19,10 @@ for col in range(data.shape[0]):
     if len(np.unique(data[:,col])) == 1:
         col_space += [col]
 
-def manhattan(a, b):
+def manhattan(a, b, factor):
     rows = len([x for x in row_space if min(a[0], b[0]) < x < max(a[0], b[0])])
     cols = len([x for x in col_space if min(a[1], b[1]) < x < max(a[1], b[1])])
-    return sum(abs(val1-val2) for val1, val2 in zip(a,b)) + (1000000-1)*(rows + cols)
+    return sum(abs(val1-val2) for val1, val2 in zip(a,b)) + (factor-1)*(rows + cols)
 
 # for row in range(data.shape[0] - 1, 0, -1):
 #     if len(np.unique(data[row])) == 1:
@@ -36,6 +36,6 @@ galaxies = [tuple(x) for x in np.argwhere(data.find('#') == 0)]
 
 acc = 0
 for a,b in itertools.combinations_with_replacement(galaxies, 2):
-    d = manhattan(a, b)
+    d = manhattan(a, b, 1e6)
     print(f'{a}-{b}: {d}')
     acc += d
