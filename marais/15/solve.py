81a1796e56abd9996d47c85b459b7ae758e6a4f2
Marais Neethling <marais@synthesis.co.za>
2023-12-19 15:48:54 +0200

    Marais - Day 15

--- marais/15/solve.py
+++ marais/15/solve.py
@@ -14,13 +14,81 @@ def my_hash(string):
     return acc
 
 
+class Lens:
+    def __init__(self, label: str, fl: int):
+        self.label = label
+        self.fl = fl
+
+    def __hash__(self):
+        return my_hash(self.label)
+
+    def __cmp__(self, other):
+        return self.label == other.label
+
+    def __str__(self):
+        return f"{self.label}={self.fl}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.label == other.label
+
+
+class hash_map:
+    def __init__(self, size=256):
+        self.size = size
+        self.map = [[] for i in range(size)]
+
+    def add(self, l: Lens):
+        box_idx = l.__hash__()
+        if l in self.map[box_idx]:
+            l_idx = self.map[box_idx].index(l)
+            self.map[box_idx][l_idx] = l
+        else:
+            self.map[box_idx].append(l)
+
+    def remove(self, l: Lens):
+        box_idx = l.__hash__()
+        if l in self.map[box_idx]:
+            l_idx = self.map[box_idx].index(l)
+            self.map[box_idx].pop(l_idx)
+
+    def __str__(self):
+        return str(self.map)
+
+def solve2(strings):
+    hm = hash_map()
+    for s in strings:
+        if '=' in s:
+            label, fl = s.split('=')
+            print(f"Label: {label}, fl: {fl}")
+            l = Lens(label, int(fl))
+            hm.add(l)
+            # print(hm)
+        else:
+            label = s.split('-')[0]
+            print(f"Label: {label}")
+            hm.remove(Lens(label, 0))
+            # print(hm)
+
+    acc = 0
+    for i, b in enumerate(hm.map):
+        for j, l in enumerate(b):
+            prod = 1
+            prod *= (i+1)
+            prod *= (j+1)
+            prod *= l.fl
+            acc += prod
+            print(f"prod: {prod}")
+    return acc
+
 if __name__ == '__main__':
 
     input = read_input('input.txt')
-    # lines = [block.split('\n') for block in input]
     strings = input.split(',')
     ans = sum([my_hash(string) for string in strings])
     print(f"Answer 1: {ans}")
-    #
-    # ans2 = solve2(lines, 1000000000)
-    # print(f"Answer 2: {ans2}")
+
+    ans2 = solve2(strings)
+    print(f"Answer 2: {ans2}")
