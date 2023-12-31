34bf15c8d5be31fd9666417cc48f95c381e5e9a3
Bhavesh Sooka <bsooka@gmail.com>
2024-01-01 01:09:25 +0200

    wip: dfs for workflows

--- bhavesh/rust/src/days/day19.rs
+++ bhavesh/rust/src/days/day19.rs
@@ -52,12 +52,48 @@ fn sample_solution_part1() -> String {
 fn sample_solution_part2() -> String {
     let input_data = String::from("px{a<2006:qkq,m>2090:A,rfg}\npv{a>1716:R,A}\nlnx{m>1548:A,A}\nrfg{s<537:gd,x>2440:R,A}\nqs{s>3448:A,lnx}\nqkq{x<1416:A,crn}\ncrn{x>2662:A,R}\nin{s<1351:px,qqz}\nqqz{s>2770:qs,m<1801:hdj,R}\ngd{a>3333:R,R}\nhdj{m>838:A,pv}\n\n{x=787,m=2655,a=1222,s=2876}\n{x=1679,m=44,a=2067,s=496}\n{x=2036,m=264,a=79,s=2244}\n{x=2461,m=1339,a=466,s=291}\n{x=2127,m=1623,a=2188,s=1013}\n");
 
+    let split_str = input_data
+        .split("\n\n")
+        .filter(|x| !x.is_empty())
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>();
+
+    let workflows = parse_workflows(&split_str[0]);
+
+    let paths: Vec<Vec<String>> = traverse_workflows(&workflows, &String::from("in"));
+
+    println!("{:?}", paths);
+
     part2(&input_data)
 }
 
 // --------------------------------------------------------------------------------------
 // Actual solution
 // --------------------------------------------------------------------------------------
+fn traverse_workflows(workflows: &WorkFlows, key: &Key) -> Vec<Vec<String>> {
+    if key == "R" || key == "A" {
+        return vec![vec![key.clone()]];
+    }
+
+    let mut new_paths: Vec<Vec<String>> = Vec::new();
+
+    for rule in workflows.get(key).unwrap() {
+        println!("rule {:?}", rule);
+
+        let next_paths = traverse_workflows(workflows, &rule.result);
+        for next_path in next_paths {
+            let mut sub_path: Vec<String> = vec![key.clone()];
+            sub_path.extend(next_path);
+            new_paths.push(sub_path.clone());
+        }
+    }
+    new_paths
+        .iter()
+        .filter(|x| x.to_vec().contains(&"A".to_string()))
+        .map(|x| x.to_vec())
+        .collect::<Vec<Vec<String>>>()
+}
+
 fn sum_valid_parts(parts: &Parts, workflows: &WorkFlows) -> i64 {
     parts
         .iter()
